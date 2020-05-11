d08bf78c473588e145582025cd4772064e358d3c
Rizku Aulia Safitri <64572670+rizkyaulias@users.noreply.github.com>
2020-05-11 21:11:28 +0700

    Create Jobsheet 3 C++.cpp
    Buatlah program penjumlahan matriks dua dimensi dengan menggunakan NIM dua orang mahasiswa

---
+++ Jobsheet 3 C++.cpp
@@ -0,0 +1,30 @@
+ #include <iostream>
+using namespace std ; 
+
+int main (){
+	int i,j ;
+	int matriks1[2][2],matriks2[2][2],jumlah [2][2];
+	cout << "MATRIKS 1"<<endl ;
+	for (i=0 ; i<2 ;i++){
+		for(j=0 ; j<2 ;j++){
+			cout << "masukkan elemen"<<"["<<i<<"]["<<j<<"]= " ;
+			cin >> matriks1[i][j];
+		}
+		}
+		cout <<endl ;
+		cout << "MATRIKS 2"<<endl ;
+	for (i=0 ; i<2 ;i++){
+		for(j=0 ; j<2 ;j++){
+			cout << "masukkan elemen"<<"["<<i<<"]["<<j<<"]= " ;
+			cin >> matriks2[i][j];
+		}
+		}
+		cout << endl ;
+		for (i=0 ; i<2 ;i++){
+		for(j=0 ; j<2 ;j++){
+			jumlah [i][j]= matriks1[i][j]+ matriks2[i][j] ;
+			cout <<"["<<jumlah [i][j]<<"]" ;
+		}
+		cout <<endl ;
+	}
+}
