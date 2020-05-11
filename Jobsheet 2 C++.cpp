c3c25f5e54e9fe830cd079544d4caa10932d939b
Rizku Aulia Safitri <64572670+rizkyaulias@users.noreply.github.com>
2020-05-11 21:07:47 +0700

    Add files via upload
    Buatlah array dua dimensi yang menampilkan NIM dari mahasiswa yang berbeda

---
+++ Jobsheet 2 C++.cpp
@@ -0,0 +1,14 @@
+ #include <iostream>
+using namespace std ;
+
+int main (){
+int nim[2][2] ;
+nim [0][0] = 6 ;
+nim [0][1] = 4 ;
+nim [1][0] = 6 ;
+nim [1][1] = 4 ;
+
+cout <<"\n"<<nim[0][0]<<" " <<nim[0][1];
+cout <<"\n"<<nim[1][0]<<" " <<nim[1][1];
+}
+
