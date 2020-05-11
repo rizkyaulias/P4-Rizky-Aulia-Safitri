926e62ac81a1302d49ca01b37c3938a3c8afc7f1
Rizku Aulia Safitri <64572670+rizkyaulias@users.noreply.github.com>
2020-05-11 21:01:39 +0700

    Create Jobsheet 1 C.c
    Buatlah program array satu dimensi yang menampilkan NIM masing-masing mahasiswa

---
+++ Jobsheet 1 C.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (){
+int nim[6] = {1,2,3,4,5,6} ;
+
+printf("NAMA      : Rizky Aulia Safitri \n" );
+printf("NIM       : F1B019126 \n" );
+printf("KELOMPOK  : 21\n" );
+
+    printf ("Nim ke-1 = %i\n",nim[0]);
+	printf ("Nim ke-2 = %i\n",nim[1]);
+	printf ("Nim ke-3 = %i\n",nim[2]);
+	printf ("Nim ke-4 = %i\n",nim[3]);
+	printf ("Nim ke-5 = %i\n",nim[4]);
+	printf ("Nim ke-6 = %i\n",nim[5]);	printf ("Nim ke-7 = %i\n",nim[6]);
+}
