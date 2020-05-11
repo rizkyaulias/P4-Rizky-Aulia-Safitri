e4a0c5b13d59538392067816687e18f4059e6c0b
Rizku Aulia Safitri <64572670+rizkyaulias@users.noreply.github.com>
2020-05-11 21:10:22 +0700

    Create Jobsheet 2 C.c

---
+++ Jobsheet 2 C.c
@@ -0,0 +1,15 @@
+ #include <stdio.h>
+
+
+int main (){
+int nim[2][2] ;
+nim [0][0] = 6 ;
+nim [0][1] = 4 ;
+nim [1][0] = 6 ;
+nim [1][1] = 4 ;
+
+printf ("Nim [0][0] = %i\n",nim[0][0]);
+printf ("Nim [0][1]= %i\n",nim[0][1]);
+printf ("Nim [1][0]= %i\n",nim[1][0]);
+printf ("Nim [1][1] = %i\n",nim[1][1]);
+}
