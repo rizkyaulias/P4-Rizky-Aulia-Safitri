b436a43d52a0997f27b7daaab9c50eec376fea1c
Rizku Aulia Safitri <64572670+rizkyaulias@users.noreply.github.com>
2020-05-12 10:44:30 +0700

    Create Jobsheet 7 C++.cpp
    Membuat program mengisi nilai array dengan perulangan dengan index array dan batas

---
+++ Jobsheet 7 C++.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std ; 
+
+int main (){
+	int i ;
+	int matriks[6] ;
+	 for (i=1 ; i<=6 ;i++){
+	 	cout << "Nilai ke-" << i<< " = " ;
+	 	cin >> matriks[i] ;
+	 }
+	 cout <<endl ; 
+}
