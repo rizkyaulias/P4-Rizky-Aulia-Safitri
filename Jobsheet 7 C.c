747680aedbb61f245155cfb9f71003873e09a3e9
Rizku Aulia Safitri <64572670+rizkyaulias@users.noreply.github.com>
2020-05-12 10:45:14 +0700

    Create Jobsheet 7 C.c
     Membuat program mengisi nilai array dengan perulangan dengan index array dan batas

---
+++ Jobsheet 7 C.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+	int i ;
+	int matriks[6] ;
+	 for (i=1 ; i<=6 ;i++){
+	 	printf ("Masukkan Nilai ke- %d = ",i) ;
+	 	scanf("%i",&matriks[i]) ;
+	 }
+	 printf("\n") ;
+
+}
