1a0e790269e73ad875283076abe44afebad80644
Rizku Aulia Safitri <64572670+rizkyaulias@users.noreply.github.com>
2020-05-12 10:47:23 +0700

    Create Jobsheet 8 C++.cpp
    Membuat program mengisi nilai array dan menampilkannya dengan perulangan dengan indeks array

---
+++ Jobsheet 8 C++.cpp
@@ -0,0 +1,16 @@
+ #include <iostream>
+using namespace std ; 
+
+int main (){
+	int i ;
+	int matriks[6] ;
+	 for (i=1 ; i<=6 ;i++){
+	 	cout << "Masukkan nilai ke-" << i<< " = " ;
+	 	cin >> matriks[i] ;
+	 }
+	 cout <<endl ; 
+	  for (i=1 ; i<=6 ;i++){
+	 	cout << "Nilai ke-" << i<< " = "<< matriks[i] <<endl ;
+	 	
+	 }
+}
